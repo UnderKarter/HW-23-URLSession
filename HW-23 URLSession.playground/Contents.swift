7b97be16dac1060cdce8afc0ea356babddef17c5
Semenov Petr <64535714+UnderKarter@users.noreply.github.com>
2022-08-16 00:07:45 +0300

    Extra task

--- HW-23 URLSession.playground/Contents.swift
+++ HW-23 URLSession.playground/Contents.swift
@@ -26,10 +26,25 @@ func getData(urlRequest: String) {
         
     }.resume()
 }
-
+// MARK: - Basic task
 let baseWeatherUrl = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?"
 let nineDayEngWeather = baseWeatherUrl + "dataType=fnd&lang=eng"
 let nineDayChWeather = baseWeatherUrl + "dataType=fnd&lang=tc"
 
 getData(urlRequest: nineDayEngWeather)
 getData(urlRequest: nineDayChWeather)
+
+// MARK: - Extra task
+let baseMarvelUrl = "http://gateway.marvel.com/v1/public/comics/20/characters?ts=1&apikey="
+let publicKey = "d65777e97313d67a6e9b67aedd1082f6"
+let privateKey = "c125b0157f48a2082a05783f47afb736e3cfffc8"
+let ts = "1"
+
+func md5Hash(_ source: String) -> String {
+    return Insecure.MD5.hash(data: source.data(using: .utf8)!).map { String(format: "%02hhx", $0) }.joined()
+}
+
+let hash = md5Hash(ts + privateKey + publicKey)
+let endpointUrl = baseMarvelUrl + publicKey + "&hash=" + hash
+
+getData(urlRequest: endpointUrl)
