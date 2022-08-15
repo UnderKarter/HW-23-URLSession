0f4ce1e43ade634d404aa970beed162239c5ff42
Semenov Petr <64535714+UnderKarter@users.noreply.github.com>
2022-08-15 23:53:15 +0300

    Add basic request to the network

--- HW-23 URLSession.playground/Contents.swift
+++ HW-23 URLSession.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
+let configureSession = URLSessionConfiguration.default
+configureSession.allowsCellularAccess = true
+configureSession.waitsForConnectivity = true
+let session = URLSession(configuration: configureSession)
 
+func getData(urlRequest: String) {
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else { return }
+    
+    session.dataTask(with: url) { (data, response, error) in
+        if error != nil {
+            print("Error is: \(error?.localizedDescription ?? "")")
+        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            print("Server responce is: \(response.statusCode)")
+            
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+            print("Our data is: \(dataAsString ?? "Nothing")")
+            
+        } else if let response = response as? HTTPURLResponse {
+            print("Server responce is: \(response.statusCode)")
+            return
+        }
+        
+    }.resume()
+}
